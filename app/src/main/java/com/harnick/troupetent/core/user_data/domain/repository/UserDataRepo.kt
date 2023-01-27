25d54dd70048938d0f38733ab1d54e5bc194cc56
har-nick <56137506+har-nick@users.noreply.github.com>
2023-01-27 07:00:43 +0000

    Added token shorthand functions because I'm lazy

--- app/src/main/java/com/harnick/troupetent/core/user_data/domain/repository/UserDataRepo.kt
+++ app/src/main/java/com/harnick/troupetent/core/user_data/domain/repository/UserDataRepo.kt
@@ -6,6 +6,8 @@ import kotlinx.coroutines.flow.Flow
 interface UserDataRepo {
 	fun getLoginStatus(): Flow<Boolean>
 	
+	suspend fun getUserToken(): Pair<ByteArray, ByteArray>?
+	
 	fun loadUserData(): Flow<BandcampUserData>
 	
 	suspend fun updateUserData(userData: BandcampUserData)
