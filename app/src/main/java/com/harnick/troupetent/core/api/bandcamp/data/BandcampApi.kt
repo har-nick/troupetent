e45b61f6b5369fb13e2083f4e293819614e5be1d
har-nick <56137506+har-nick@users.noreply.github.com>
2023-01-27 06:30:39 +0000

    Collated Bandcamp API functions

--- app/src/main/java/com/harnick/troupetent/core/api/bandcamp/data/BandcampApi.kt
+++ app/src/main/java/com/harnick/troupetent/core/api/bandcamp/data/BandcampApi.kt
@@ -2,6 +2,8 @@ package com.harnick.troupetent.core.api.bandcamp.data
 
 import com.harnick.troupetent.core.api.bandcamp.data.dto.ErrorResponseEntity
 import com.harnick.troupetent.core.util.BandcampConstants
+import com.harnick.troupetent.library.data.dto.collection_items.BandcampCollectionItemsResponseEntity
+import com.harnick.troupetent.library.data.dto.collection_items.CollectionItemsRequestEntity
 import com.harnick.troupetent.library.data.dto.collection_summary.CollectionSummaryResponseEntity
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -23,11 +25,11 @@ class BandcampApi @Inject constructor(
 				header("Cookie", "identity=$token")
 			}
 			
-			hoistedResponse = collectionEndpointResponse
-			
 			if (collectionEndpointResponse.status != HttpStatusCode.OK) {
 				throw Exception(collectionEndpointResponse.status.description)
 			}
+			
+			hoistedResponse = collectionEndpointResponse
 			
 			return collectionEndpointResponse.body()
 		} catch (e: SerializationException) {
@@ -39,7 +41,43 @@ class BandcampApi @Inject constructor(
 			
 			throw Exception("${errorResponseEntity?.errorMessage}")
 		} catch (e: SerializationException) {
-			throw SerializationException("Failed to serialize Collection Summary")
+			throw SerializationException("Failed to serialize Collection Summary. Let a dev know!")
+		}
+	}
+	
+	suspend fun fetchCollectionItems(
+		fanId: Long,
+		itemCount: Long,
+		token: String
+	): BandcampCollectionItemsResponseEntity {
+		var hoistedResponse: HttpResponse? = null
+		
+		try {
+			val olderThanToken = "${System.currentTimeMillis()}::a::"
+			
+			val itemsEndpointResponse = client.post(BandcampConstants.ITEMS_URL) {
+				header("Cookie", "identity=$token")
+				contentType(ContentType.Application.Json)
+				setBody(CollectionItemsRequestEntity(fanId, olderThanToken, itemCount))
+			}
+			
+			if (itemsEndpointResponse.status != HttpStatusCode.OK) {
+				throw Exception(itemsEndpointResponse.status.description)
+			}
+			
+			hoistedResponse = itemsEndpointResponse
+			
+			return itemsEndpointResponse.body()
+		} catch (e: SerializationException) {
+			var errorResponseEntity: ErrorResponseEntity? = null
+			
+			if (hoistedResponse != null) {
+				errorResponseEntity = hoistedResponse.body()
+			}
+			
+			throw Exception("${errorResponseEntity?.errorMessage}")
+		} catch (e: SerializationException) {
+			throw SerializationException("Failed to serialize Collection Items. Let a dev know!")
 		}
 	}
 	
