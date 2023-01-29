edbe2677b6e77c495dc5538303f50e1f75845758
har-nick <56137506+har-nick@users.noreply.github.com>
2023-01-29 21:34:45 +0000

    Improved library notifier function

--- app/src/main/java/com/harnick/troupetent/library/presentation/components/StatusMessage.kt
+++ app/src/main/java/com/harnick/troupetent/library/presentation/components/StatusMessage.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.harnick.troupetent.authentification.presentation.screens.onboarding.components.SquaredButton
@@ -16,23 +15,17 @@ fun LibraryStateNotififer(
 	error: String?,
 	handleEvent: (LibraryEvent) -> Unit
 ) {
-	Column(
-		Modifier.fillMaxSize(),
-		verticalArrangement = Arrangement.Center,
-		horizontalAlignment = Alignment.CenterHorizontally
-	) {
-		if (status != null) {
-			CircularProgressIndicator()
+	if (status != null) {
+		CircularProgressIndicator()
+	}
+	
+	Box(Modifier.padding(20.dp)) {
+		if (status != null || error != null) {
+			Text((status ?: error)!!)
 		}
-		
-		Box(Modifier.padding(20.dp)) {
-			if (status != null || error != null) {
-				Text((status ?: error)!!)
-			}
-		}
-		
-		if (error != null) {
-			SquaredButton({ handleEvent(LibraryEvent.ResyncLibrary) }, minWidth = 0.dp, "Resync Library")
-		}
+	}
+	
+	if (error != null) {
+		SquaredButton({ handleEvent(LibraryEvent.ResyncLibrary) }, minWidth = 0.dp, "Resync Library")
 	}
 }
