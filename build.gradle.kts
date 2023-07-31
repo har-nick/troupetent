0d519932ff6adbaf7a036cac39269c89f0766657
Andrew Harnick <56137506+har-nick@users.noreply.github.com>
2023-07-31 16:04:10 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.android.versioning).apply(false)
+    alias(libs.plugins.compose.multiplatform).apply(false)
+    alias(libs.plugins.kotlinx.serialization).apply(false)
+    alias(libs.plugins.sqldelight).apply(false)
+    alias(libs.plugins.moko.resources.generator).apply(false)
 }
 
 tasks.register("clean", Delete::class) {
